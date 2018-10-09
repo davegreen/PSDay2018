48761b737ce1eb22abe87c8551ff1c2b3c51da3c
Dave Green <david.green@tookitaway.co.uk>
2018-10-09 20:20:57 +0100

    include/exclude example

--- Demo.ps1
+++ Demo.ps1
@@ -16,6 +16,10 @@ Get-TestSuiteData -TestSuite BasicExample2.TestSuite
 Invoke-TestSuite -TestSuite BasicExample2.TestSuite -Environment prod -TestResultShow All
 Invoke-TestSuite -TestSuite BasicExample2.TestSuite -Environment test -TestResultShow All
 
+# 3
+Invoke-TestSuite -TestSuite BasicExample2.TestSuite -ExcludeTests Process
+Invoke-TestSuite -TestSuite BasicExample2.TestSuite -IncludeTests Service
+
 # Report
 Invoke-TestSuite -TestSuite BasicExample2.TestSuite -OutputHTML -OpenHTML
 
