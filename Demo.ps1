2b2f98ea1a9c09cba374e6e8d3fab926ba88d6b9
Dave Green <david.green@tookitaway.co.uk>
2018-10-10 05:53:26 +0100

    small example change (use Get-testsuitedata)

--- Demo.ps1
+++ Demo.ps1
@@ -9,19 +9,25 @@ Get-TestSuite
 
 # 1
 Get-TestSuiteData -TestSuite BasicExample.TestSuite
+
 Invoke-TestSuite -TestSuite BasicExample.TestSuite -TestResultShow All
 
 # 2
 Get-TestSuiteData -TestSuite BasicExample2.TestSuite
 Invoke-TestSuite -TestSuite BasicExample2.TestSuite -Environment prod -TestResultShow All
+
 Invoke-TestSuite -TestSuite BasicExample2.TestSuite -Environment test -TestResultShow All
 
 # 3
 Invoke-TestSuite -TestSuite BasicExample2.TestSuite -ExcludeTests Process
+
 Invoke-TestSuite -TestSuite BasicExample2.TestSuite -IncludeTests Service
 
 # Report
 Invoke-TestSuite -TestSuite BasicExample2.TestSuite -OutputHTML -OpenHTML
 
+
 # O365
-Invoke-TestSuite -TestSuite O365.TestSuite -OutputHTML
+Get-TestSuiteData -TestSuite O365.TestSuite
+
+Invoke-TestSuite -TestSuite O365.TestSuite -OutputHTML -IncludeTests ExchangeOnline
